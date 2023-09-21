1b66b5f55be3ad7ef85c057b5d120c82daffd387
Isaac Garcia Peveri <isacco1975gp@gmail.com>
2023-09-21 20:30:28 +0200

    Add project

---
+++ ArduinoUNO_Sketch/ArduinoUNO_Sketch.ino
@@ -0,0 +1,67 @@
+/*
+ **********************************************************************
+ * Name:                                                              *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *
+ * Created: 09.03.2023 10:01:49                                       *
+ * Author:  Isaac Garcia Peveri                                       *
+ *          isacco1975gp@gmail.com                                    *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *
+ **********************************************************************
+*/
+
+#pragma region "Working-Storage"
+const int GREEN_LED = 2;
+const int YELLOW_LED = 3;
+const int RED_LED = 4;
+String incomingMessage;
+#pragma endregion
+
+void setup()
+{  
+   Serial.begin(115200);
+}
+
+void loop()
+{ 
+   if (Serial.available()) 
+   {
+      incomingMessage = Serial.readString();
+      Serial.println(incomingMessage);
+      Serial.println(incomingMessage.indexOf("ON1"));
+      Serial.println(incomingMessage.indexOf("ON2"));
+      Serial.println(incomingMessage.indexOf("ON3"));
+      Serial.println(incomingMessage.indexOf("OFF1"));
+      Serial.println(incomingMessage.indexOf("OFF2"));
+      Serial.println(incomingMessage.indexOf("OFF3"));
+     
+      if (incomingMessage.indexOf("ON1") > 0)
+      {
+         digitalWrite(GREEN_LED, HIGH);
+      } 
+      if (incomingMessage.indexOf("ON2") > 0)
+      {
+         digitalWrite(YELLOW_LED, HIGH);        
+      } 
+      if (incomingMessage.indexOf("ON3") > 0)
+      {
+         digitalWrite(RED_LED, HIGH);        
+      } 
+      if (incomingMessage.indexOf("OFF1") > 0)
+      {
+         digitalWrite(GREEN_LED, LOW);        
+      } 
+      if (incomingMessage.indexOf("OFF2") > 0)
+      {
+         digitalWrite(YELLOW_LED, LOW);                
+      } 
+      if (incomingMessage.indexOf("OFF3") > 0)
+      {
+         digitalWrite(RED_LED, LOW);                
+      }                  
+
+      Serial.flush();
+   }
+
+   //delay(500);
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ARDUINO MVC
+A simple C# ASP.NET MVC Web interface controlling Arduino
+via VB.NET TCP-IP Server.
+The server is the same used to do the same in COBOL, with some changes.
